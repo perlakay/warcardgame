f031b709097127173e1b0acedcf1b46849365254
perlakay <perla.kay22@gmail.com>
2025-01-09 16:22:23 -0500

    war card game initial commit

---
+++ war card game/war_card_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_card_gameApp.swift
+//  war card game
+//
+//  Created by Perly Dahan on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
